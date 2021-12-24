dbd2fe12754fdc6d0a4c269315ea05b9ae9c0930
Danielle Parker <79360823+carouselcolours@users.noreply.github.com>
2021-12-23 17:30:56 -0700

    started from scratch

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by
+          
+            Danielle Parker
+          
+          and is{" "}
+          <a
+            href="https://github.com/carouselcolours/reactweather"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
